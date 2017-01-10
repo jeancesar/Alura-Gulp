160286ecf4333789eeeef740fadaf8769cef3811
Jean Cesar <jean.batista@cwi.com.br>
2017-01-10 17:26:30 -0200

    Adição de tarefas

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp')
 ,imagemin = require('gulp-imagemin')
 ,clean = require('gulp-clean');
 
-gulp.task('copy',['clean'], function() {
-	return gulp.src('src/**/*')
-	.pipe(gulp.dest('dist'));
-});
-
 gulp.task('clean', function() {
 	return gulp.src('dist')
 	.pipe(clean());
+});
+
+gulp.task('copy',['clean'], function() {
+	return gulp.src('src/**/*')
+	.pipe(gulp.dest('dist'));
 });
 
 gulp.task('build-img', ['copy'], function() {
