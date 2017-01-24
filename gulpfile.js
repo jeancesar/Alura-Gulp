737facd09fbf207804682dbbbdaed556b51613d9
Jean Cesar <jean.batista@cwi.com.br>
2017-01-24 17:12:49 -0200

    aula minificacao e debug

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp')
 ,imagemin = require('gulp-imagemin')
-,clean = require('gulp-clean');
+,clean = require('gulp-clean')
+,concat = require('gulp-concat')
+,htmlReplace = require('gulp-html-replace')
+,uglify = require('gulp-uglify')
+,usemin = require('gulp-usemin')
+,cssmin = require('gulp-cssmin')
+,browserSync = require('browser-sync')
+,jshint = require('gulp-jshint')
+,jshintStylish = require('jshint-stylish');
+
+gulp.task('default', ['copy'], function() {
+	gulp.start('build-img', 'usemin');
+});
 
 gulp.task('clean', function() {
 	return gulp.src('dist')
@@ -12,8 +24,33 @@ gulp.task('copy',['clean'], function() {
 	.pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
 	gulp.src('dist/img/**/*')
 	.pipe(imagemin())
 	.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+  return gulp.src('dist/**/*.html')
+    .pipe(usemin({
+      js: [uglify],
+	  css: [cssmin]
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+
+	gulp.watch('src/js/*.js').on('change', function(event){
+		gulp.src(event.path)
+			.pipe(jshint())
+			.pipe(jshint.reporter(jshintStylish));
+	});
+
+    gulp.watch('src/**/*').on('change', browserSync.reload);
 });
