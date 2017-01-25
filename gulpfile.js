d254557f20e5b1e2e23f8b6f235d76d142cdc268
Jean Cesar <neo.jean@gmail.com>
2017-01-25 15:45:19 -0200

    Conclusão do curso

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,10 @@ var gulp = require('gulp')
 ,cssmin = require('gulp-cssmin')
 ,browserSync = require('browser-sync')
 ,jshint = require('gulp-jshint')
-,jshintStylish = require('jshint-stylish');
+,jshintStylish = require('jshint-stylish')
+,csslint = require('gulp-csslint')
+,autoprefixer = require('gulp-autoprefixer')
+,less = require('gulp-less');
 
 gulp.task('default', ['copy'], function() {
 	gulp.start('build-img', 'usemin');
@@ -31,26 +34,42 @@ gulp.task('build-img', function() {
 });
 
 gulp.task('usemin', function() {
-  return gulp.src('dist/**/*.html')
-    .pipe(usemin({
-      js: [uglify],
-	  css: [cssmin]
-    }))
-    .pipe(gulp.dest('dist'));
+	return gulp.src('dist/**/*.html')
+	.pipe(usemin({
+		js: [uglify],
+		css: [autoprefixer, cssmin]
+	}))
+	.pipe(gulp.dest('dist'));
 });
 
 gulp.task('server', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'src'
-        }
-    });
+	browserSync.init({
+		server: {
+			baseDir: 'src'
+		}
+	});
 
 	gulp.watch('src/js/*.js').on('change', function(event){
 		gulp.src(event.path)
-			.pipe(jshint())
-			.pipe(jshint.reporter(jshintStylish));
+		.pipe(jshint())
+		.pipe(jshint.reporter(jshintStylish));
 	});
 
-    gulp.watch('src/**/*').on('change', browserSync.reload);
+	gulp.watch('src/css/**/*.css').on('change', function(event) {
+		console.log("Linting " + event.path);
+		gulp.src(event.path)
+		.pipe(csslint())
+		.pipe(csslint.reporter());
+	});
+
+
+	gulp.watch('src/less/**/*.less').on('change', function(event) {
+		gulp.src(event.path)
+		.pipe(less().on('error', function(erro) {
+			console.log('LESS, erro compilação: ' + erro.filename);
+			console.log(erro.message);
+		}))
+		.pipe(gulp.dest('src/css'));
+	});
+	gulp.watch('src/**/*').on('change', browserSync.reload);
 });
